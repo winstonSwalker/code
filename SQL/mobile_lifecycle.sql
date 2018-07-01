679c7228d30ee293c4854bd0af30ed67346aa802
Winston Walker <winstonSwalker@gmail.com>
2018-06-30 20:10:36 -0700

    adding queries

---
+++ SQL/mobile_lifecycle.sql
@@ -0,0 +1,81 @@
+--this query creates a lifecycle of mobile users to assess where users are being lost
+--this is going to be very time consuming and it's best to have this kind of a query run on a standard etl
+
+--"app_open" table columns:
+----id: id for a person
+----timestamp: 'yyyy-mm-dd hh:mm:ss' format of the time of the app open
+
+--"ftue" table columns:
+----id: id for a person
+----timestamp: 'yyyy-mm-dd hh:mm:ss' format of the time of the app open
+----step: which step of the ftue the user is in (for this table 1 = started ftue and 10 = finished ftue)
+
+--"engagement" table columns:
+----id: id for a person
+----timestamp: 'yyyy-mm-dd hh:mm:ss' format of the time of the app open
+----activity: which feature the user is engaging in (for this table: menu, click, use)
+
+--"payment" table columns:
+----id: id for a person
+----timestamp: 'yyyy-mm-dd hh:mm:ss' format of the time of the payment
+----revenue: $ value of payment in US dollars
+
+with install as (
+	select
+		id
+		,min(date(timestamp)) as install_date
+	from app_open
+	group by
+		id
+		)
+,ftue_start_end as(
+	select
+		id
+		,max(case when step = 1 then 1 else 0 end) as ftue_start
+		,max(case when step =10 then 1 else 0 end) as ftue_end
+	from ftue
+	group by
+		id
+		)
+,engagement_activities as(
+	select
+		id
+		,max(case when activity = 'menu' then 1 else 0 end) as eng_menu
+		,max(case when activity = 'click' then 1 else 0 end) as eng_click
+		,max(case when activity = 'use' then 1 else 0 end) as eng_use
+	from engagement
+	group by
+		id
+		)
+,customer  (
+	select
+		id
+		,1 as pay
+	from payment
+	where 1=1
+		and revenue > 0 and revenue is not null
+	group by 
+		id
+		)
+,lifecycle as (
+	select
+		a.install_date
+		,count(distinct a.id) as installs
+		,sum(coalesce(b.ftue_start,0)) as ftue_start
+		,sum(coalesce(b.ftue_end,0)) as ftue_end
+		,sum(coalesce(c.eng_menu,0)) as eng_menu
+		,sum(coalesce(c.eng_click,0)) as eng_click
+		,sum(coalesce(c.eng_use,0)) as eng_use
+		,sum(coalesce(d.pay,0)) as pay
+	from install a
+		left join ftue_start_end b
+			on a.id = b.id
+		left join engagement_activities c
+			on a.id = c.id
+		left join customer d
+			on a.id = d.id
+	group by 
+		a.install_date
+		)
+select * from lifecycle
+
